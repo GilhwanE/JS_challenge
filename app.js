600d4e1bc565ef5072f74896baa545023912a5e7
Gil Hwan <63918911+GilhwanE@users.noreply.github.com>
2021-09-30 10:54:29 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const 값을 바꾸지않는 변수
+
+const a = 5;
+const b = -5;
+
+const myName = 'Hawn';
+
+// console.log(a + b + myName);
+
+// boolean
+
+const amIFat = null; //null은 절대 자연적으로 생기지 않는다 . 비어있습니다 라는 말을 표현하기 위해
+let something; // undefined 는 값이 선언하지 않았습니다. 즉 비어있습니다와는 다른 의미 값이 없는것.
+// console.log(amIFat);
+
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'sun'];
+
+console.log(daysOfWeek[4]);
